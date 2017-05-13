b2c2c1ef2dd7b44e449d342f5657ac04b7532053
Jim Szalewski <jim.szalewski@gmail.com>
2017-05-13 17:44:48 -0600

    finished unicorn and vampire

---
+++ lib/vampire.rb
@@ -0,0 +1,25 @@
+class Vampire
+
+  def initialize(name, pet = "bat", thirsty = true)
+    @name = name
+    @pet  = pet
+    @thirsty = thirsty
+  end
+
+  def name
+    @name
+  end
+
+  def pet
+    @pet
+  end
+
+  def thirsty?
+    @thirsty == true
+  end
+
+  def drink
+    @thirsty = false
+  end
+
+end
