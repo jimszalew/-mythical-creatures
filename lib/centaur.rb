113e0beab5743712551ddc0bef36dd7be9c24064
Jim Szalewski <jim.szalewski@gmail.com>
2017-05-18 15:49:25 -0600

    working on centaur

---
+++ lib/centaur.rb
@@ -0,0 +1,44 @@
+class Centaur
+
+  attr_reader :name, :breed
+
+  def initialize(name, breed)
+    @name   = name
+    @breed  = breed
+    @mood   = 0
+  end
+
+  def shoot
+    if cranky? == true
+      "NO!"
+    else
+      @mood += 1
+      "Twang!!!"
+    end
+  end
+
+  def run
+    if cranky? == true
+      "NO!"
+    else
+      @mood += 1
+      "Clop clop clop clop!!!"
+    end
+  end
+
+  def cranky?
+    if @mood >= 3
+      true
+    else
+      false
+    end
+  end
+
+  def standing?
+    true
+  end
+
+  def sleep
+    "NO!"
+  end
+end
