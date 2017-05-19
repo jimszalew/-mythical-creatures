3bdcd5cd21d11da9d8e184efd5759915f1f7581e
Jim Szalewski <jim.szalewski@gmail.com>
2017-05-19 10:54:54 -0600

    started working on hobbit

---
+++ lib/hobbit.rb
@@ -0,0 +1,18 @@
+class Hobbit
+
+  attr_reader :name, :disposition
+
+  def initialize(name, disposition = "homebody")
+    @name = name
+    @disposition = disposition
+  end
+
+  def age
+    0
+  end
+
+  def celebrate_birthday
+    
+  end
+
+end
