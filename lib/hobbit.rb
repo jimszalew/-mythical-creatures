367a3fc82bb129189977dffeafd6f768967fbfc9
Jim Szalewski <jim.szalewski@gmail.com>
2017-05-23 04:13:11 -0600

    finished hobbit

--- lib/hobbit.rb
+++ lib/hobbit.rb
@@ -5,14 +5,34 @@ class Hobbit
   def initialize(name, disposition = "homebody")
     @name = name
     @disposition = disposition
+    @birthday = 0
   end
 
   def age
-    0
+    @birthday
   end
 
   def celebrate_birthday
-    
+    @birthday += 1
   end
 
+  def adult?
+    @birthday >= 33
+  end
+
+  def old?
+    @birthday >= 101
+  end
+
+  def has_ring?
+    if @name == "Frodo"
+      true
+    else
+      false
+    end
+  end
+
+  def is_short?
+    true
+  end
 end
--- test/hobbit_test.rb
+++ test/hobbit_test.rb
@@ -34,7 +34,6 @@ class HobbitTest < Minitest::Test
   end
 
   def test_is_considered_a_child_at_32
-    skip
     hobbit = Hobbit.new('Gerontius')
     32.times do
       hobbit.celebrate_birthday
@@ -43,7 +42,6 @@ class HobbitTest < Minitest::Test
   end
 
   def test_comes_of_age_at_33
-    skip
     hobbit = Hobbit.new('Otho')
     33.times do
       hobbit.celebrate_birthday
@@ -56,24 +54,23 @@ class HobbitTest < Minitest::Test
   end
 
   def test_is_old_at_age_of_101
-    skip
-    # create a hobbit
-    # have hobbit age 101 years
-    # check that hobbit.old? returns true
+    hobbit = Hobbit.new("Smiegel")
+    101.times do
+      hobbit.celebrate_birthday
+    end
+    assert hobbit.old?
   end
 
   def test_hobbit_has_the_ring_if_its_name_is_frodo
-    skip
-    # create a hobbit named Frodo
-    # create a second hobbit named Sam
-    # check that .has_ring? for Frodo returns true
-    # check that .has_ring? for Sam returns false
+    hobbit1 = Hobbit.new("Frodo")
+    hobbit2 = Hobbit.new("Sam")
+    assert hobbit1.has_ring?
+    refute hobbit2.has_ring?
   end
 
   def test_hobbits_are_short
-    skip
-    # create a hobbit
-    # check that .is_short? returns true
+    hobbit = Hobbit.new("Shorty")
+    assert hobbit.is_short?
   end
 
 end
